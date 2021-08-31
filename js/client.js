10f5274e8881f7751cb754dd20c7ec6809bc116e
lhernandez19 <diegotapia1987@gmail.com>
2021-08-30 22:07:38 -0500

    first commit

---
+++ js/client.js
@@ -0,0 +1,24 @@
+$(function(){
+    var toast = new Audio('media/toast.wav');
+
+    $('.code').on('click', function(e) {
+        e.preventDefault();
+        toast.pause();
+        toast.currentTime = 0;
+        toast.play();
+
+        let product = $(this).data('product');
+        let discountCode = $(this).data('code');
+
+        $('#product').html(product);
+        $('#code').html(discountCode);
+
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+});
+
+$(document).on('keydown', (e) => {
+    if (e.key == 'Escape') {
+        $('#toast').toast('hide');
+    }
+});
